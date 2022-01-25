d970c336071c419133badea52e3bb1b2e8b10520
Lordprime77 <courtlandmoore2005@gmail.com>
2022-01-25 02:24:17 -0500

    shii i forgor these
    unlucky

---
+++ templates/tfa_gun_sounds.lua
@@ -0,0 +1,10 @@
+--go back to the .qc file in the models/weapons/whatever extra folders you use/decompiled folder/v_gun.qc
+
+sound.Add(
+{
+	name = "",
+	channel = CHAN_ITEM,
+	volume =  1.0,
+	soundlevel = 80,
+	sound = "",
+})
